d52657d4848fb3bfb156ab521870a0824fd930f4
Sam Morris <63416066+samuel7morris@users.noreply.github.com>
2020-08-26 13:38:21 -0600

    added sort by phone number

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # employee-directory
 
 this program uses react to create a responsive search function that sorts names alphabetically. It uses several components to simplify and section out the code to make it more readable. 
+
+![alt text](screenshot.png)
--- src/components/search.js
+++ src/components/search.js
@@ -4,8 +4,10 @@ import React from 'react';
 function Search({handleSearch}) {
   return (
     <div className="App">
-         <input className="input" placeholder="search employee here"  onChange = {event => handleSearch(event)}></input>
-         <button className="search">Search</button>
+         <input className="input m-3 col-md-3" placeholder="search by employee here"  onChange = {event => handleSearch(event)}></input>
+         <br></br>
+         <input className="input mx-3 col-md-3" placeholder="search by phone here"  onChange = {event => handleSearch(event)}></input>
+
     </div>
   );
 }
